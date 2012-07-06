0647e75db14c121feeeece6711ad1bc19141dcb4
Jan Bernacki <releu@me.com>
2012-07-06 15:52:35 +0400

    fix app config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Mgu::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -49,7 +49,8 @@ Mgu::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'v1.parallel.ru' }
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
