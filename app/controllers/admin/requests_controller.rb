6bf74d3a866d82cd3b6b3b4e6b788064ac5f7af1
Jan Bernacki <releu@me.com>
2012-07-12 12:43:40 +0400

    refactor requests controller

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -11,27 +11,27 @@ module Admin
     def activate
       @request = find_request(params[:request_id])
       if @request.activate
-        redirect_to [:admin, @request]
+        redirect_to_request(@request)
       else
-        redirect_to [:admin, @request], alert: @request.errors
+        redirect_to_request_with_alert(@request)
       end
     end
     
     def decline
       @request = find_request(params[:request_id])
       if @request.decline
-        redirect_to [:admin, @request]
+        redirect_to_request(@request)
       else
-        redirect_to [:admin, @request], alert: @request.errors
+        redirect_to_request_with_alert(@request)
       end
     end
     
     def finish
       @request = find_request(params[:request_id])
       if @request.finish
-        redirect_to [:admin, @request]
+        redirect_to_request(@request)
       else
-        redirect_to [:admin, @request], alert: @request.errors
+        redirect_to_request_with_alert(@request)
       end
     end
     
@@ -39,6 +39,14 @@ module Admin
     
     def find_request(id)
       Request.find(id)
+    end
+    
+    def redirect_to_request(request)
+      request_to [:admin, request]
+    end
+    
+    def redirect_to_request_with_alert(request)
+      redirect_to [:admin, request], alert: request.errors
     end
   end
 end
