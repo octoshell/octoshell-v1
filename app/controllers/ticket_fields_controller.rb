3ef3d8aaab14e93af195fe1b992f8a753d52f001
Jan Bernacki <releu@me.com>
2012-08-10 16:44:34 +0400

    tests

--- app/controllers/ticket_fields_controller.rb
+++ app/controllers/ticket_fields_controller.rb
@@ -14,7 +14,7 @@ class TicketFieldsController < ApplicationController
   def create
     @ticket_field = TicketField.new(params[:ticket_field], as_role)
     if @ticket_field.save
-      redirect_to_index
+      redirect_to @ticket_field
     else
       render :new
     end
@@ -27,16 +27,16 @@ class TicketFieldsController < ApplicationController
   def update
     @ticket_field = find_ticket_field(params[:id])
     if @ticket_field.update_attributes(params[:ticket_field], as_role)
-      redirect_to_index
+      redirect_to @ticket_field
     else
       render :edit
     end
   end
   
   def close
-    @ticket_field = find_ticket_field(params[:ticket_field_id], as_role)
+    @ticket_field = find_ticket_field(params[:ticket_field_id])
     @ticket_field.close
-    redirect_to_index
+    redirect_to @ticket_field
   end
   
 private
---
+++ spec/requests/ticket_fields_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe 'Ticket Fields', js: true do
+  describe 'listing' do
+    let!(:ticket_field) { create(:ticket_field) }
+    
+    before do
+      login create(:admin_user)
+      visit ticket_fields_path
+    end
+    
+    it 'should show ticket field' do
+      page.should have_link(ticket_field.name)
+    end
+  end
+  
+  describe 'creating' do
+    let!(:ticket_field) { build(:ticket_field) }
+    
+    before do
+      login create(:admin_user)
+      visit new_ticket_field_path
+      fill_in 'Name', with: ticket_field.name
+      fill_in 'Hint', with: ticket_field.hint
+      click_button 'Create Ticket field'
+    end
+    
+    it 'should create ticket field' do
+      TicketField.find_by_name(ticket_field.name).should be
+    end
+  end
+  
+  describe 'updating' do
+    let!(:ticket_field) { create(:ticket_field) }
+    
+    before do
+      login create(:admin_user)
+      visit edit_ticket_field_path(ticket_field)
+      fill_in 'Name', with: 'Moo'
+      click_button 'Update Ticket field'
+    end
+    
+    it 'should update ticket' do
+      ticket_field.reload.name.should == 'Moo'
+    end
+  end
+  
+  describe 'closing' do
+    let!(:ticket_field) { create(:ticket_field) }
+    
+    before do
+      login create(:admin_user)
+      visit ticket_field_path(ticket_field)
+      click_link 'close'
+      confirm_dialog
+    end
+    
+    it 'should close ticket' do
+      ticket_field.reload.should be_closed
+    end
+  end
+end
