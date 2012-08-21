18244abdfba2345e28c7a34bc51384fe6fb2ae22
Jan Bernacki <releu@me.com>
2012-08-21 15:35:18 +0400

    finding cluster users

--- app/controllers/cluster_users_controller.rb
+++ app/controllers/cluster_users_controller.rb
@@ -1,4 +1,11 @@
 class ClusterUsersController < ApplicationController
+  before_filter :setup_default_filter, only: :index
+  
+  def index
+    @search = ClusterUser.search(params[:search])
+    @clusters = @search.page(params[:page])
+  end
+  
   def show
     @cluster_user = ClusterUser.find(params[:id])
     authorize! :show, @cluster_user
@@ -8,5 +15,9 @@ private
   
   def namespace
     admin? ? :admin : :dashboard
+  end
+  
+  def setup_default_filter
+    params[:search] ||= { state_in: ['active'] }
   end
 end
