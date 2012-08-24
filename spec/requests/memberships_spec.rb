d84e978cee4a7004533bddb1807666f15ce5bf3a
Jan Bernacki <releu@me.com>
2012-08-24 12:55:00 +0400

    membership tests

--- spec/requests/memberships_spec.rb
+++ spec/requests/memberships_spec.rb
@@ -22,4 +22,106 @@ describe 'Memberships', js: true do
       user.memberships.first.should have(1).positions
     end
   end
+  
+  describe 'listing' do
+    let!(:membership) { create(:membership) }
+    
+    context 'as admin' do
+      before do
+        login create(:admin_user)
+        visit memberships_path
+      end
+      
+      it 'should show membership' do
+        page.should have_css("#membership-#{membership.id}")
+      end
+    end
+    
+    context 'as user' do
+      before do
+        login membership.user
+        visit memberships_path
+      end
+      
+      it 'should show membership' do
+        page.should have_css("#membership-#{membership.id}")
+      end
+    end
+    
+    context 'as non authorized user' do
+      before do
+        visit memberships_path
+      end
+      
+      it 'should show login page' do
+        current_path.should == new_session_path
+      end
+    end
+  end
+  
+  describe 'showing' do
+    let!(:membership) { create(:membership) }
+    
+    context 'as admin' do
+      before do
+        login create(:admin_user)
+        visit membership_path(membership)
+      end
+      
+      it 'should show membership' do
+        page.should have_css("#membership-#{membership.id}-detail")
+      end
+    end
+    
+    context 'as user' do
+      before do
+        login membership.user
+        visit membership_path(membership)
+      end
+      
+      it 'should show membership' do
+        page.should have_css("#membership-#{membership.id}-detail")
+      end
+    end
+    
+    context 'as non authorized user' do
+      before do
+        visit membership_path(membership)
+      end
+      
+      it 'should show login page' do
+        current_path.should == new_session_path
+      end
+    end
+  end
+  
+  describe 'closing' do
+    let!(:membership) { create(:membership) }
+    
+    context 'as admin' do
+      before do
+        login create(:admin_user)
+        visit membership_path(membership)
+        click_link 'close'
+        confirm_dialog
+      end
+      
+      it 'should close membership' do
+        membership.reload.should be_closed
+      end
+    end
+    
+    context 'as user' do
+      before do
+        login membership.user
+        visit membership_path(membership)
+        click_link 'close'
+        confirm_dialog
+      end
+      
+      it 'should close membership' do
+        membership.reload.should be_closed
+      end
+    end
+  end
 end
