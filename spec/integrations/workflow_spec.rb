dac4db63a6a0c19ec7870d62b7aecf3d7d2b35c6
Jan Bernacki <releu@me.com>
2012-08-24 18:03:47 +0400

    expand

--- spec/integrations/workflow_spec.rb
+++ spec/integrations/workflow_spec.rb
@@ -108,5 +108,83 @@ describe 'Workflow', js: true do
     fill_in 'Hours', with: 10
     fill_in 'Size', with: 10
     click_button 'Create Project'
+    
+    logout
+    
+    # scene #5. Approving request
+    login admin
+    
+    click_link 'Заявки (1)'
+    click_link 'открыть'
+    click_link 'activate'
+    
+    logout
+    
+    # scene #5. Invite coworker
+    login coworker
+    
+    click_link 'Личный кабинет'
+    click_link 'Мои поручительства'
+    click_link 'Новое поручительство'
+    select 'Evrone', from: 'Organization'
+    click_button 'Create Surety'
+    
+    click_link 'Мои места работы'
+    click_link 'Новое место работы'
+    select 'Evrone', from: 'Organization'
+    fill_in 'Должность', with: 'Разработчик'
+    click_button 'Create Membership'
+    
+    click_link 'Мои публичные ключи'
+    click_link 'Новый публичный ключ'
+    fill_in 'Name', with: 'PC'
+    fill_in 'Public key', with: '=== atata'
+    click_button 'Create Credential'
+    
+    click_link 'Поддержка'
+    click_link 'Заявки (0)'
+    click_link 'Новая заявка'
+    select 'Другое', from: 'Ticket question'
+    click_button 'Продолжить'
+    fill_in 'Subject', with: 'Скан поручительства'
+    fill_in 'Message', with: 'Прикладываю файл'
+    click_button 'Create Ticket'
+    
+    logout
+    
+    login admin
+    
+    click_link 'Кабинет Администратора'
+    click_link 'Поручительства (1)'
+    click_link 'открыть'
+    click_link 'activate'
+    
+    logout
+    
+    login coworker
+    
+    click_link 'Доступы к проектам (0/0)'
+    click_link 'Новый доступ'
+    
+    within '#request_account' do
+      select 'octoshell', from: 'Project'
+      click_button 'Запросить'
+    end
+    
+    logout
+    
+    login manager
+    
+    click_link 'Доступы к проектам (1/2)'
+    within '#account-2' do
+      click_link 'открыть'
+    end
+    click_link 'activate'
+    
+    logout
+    
+    login coworker
+    
+    sleep 3
   end
 end
