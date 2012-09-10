a96dadc386aab428596e74f28038c8f480613c55
Jan Bernacki <releu@me.com>
2012-09-10 16:02:34 +0400

    fix unicorn pid

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 # Set environment to development unless something else is specified
 env = ENV["RAILS_ENV"] || "production"
-app_path = '/var/www/octoshell-extend/current'
+app_path = '/var/www/octoshell/current'
 rails_env = env
 # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
 # documentation.
