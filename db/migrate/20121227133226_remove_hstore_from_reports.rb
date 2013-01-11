ef474bdb12e0d91385df27013dabe6c4bb2a0eff
Jan Bernacki <releu@me.com>
2013-01-11 11:22:28 +0400

    remove hstore migrations

--- db/migrate/20121227133226_remove_hstore_from_reports.rb
+++
@@ -1,8 +0,0 @@
-class RemoveHstoreFromReports < ActiveRecord::Migration
-  def change
-    remove_column :reports, :points
-    add_column :reports, :illustrations_points, :integer
-    add_column :reports, :statement_points, :integer
-    add_column :reports, :summary_points, :integer
-  end
-end
