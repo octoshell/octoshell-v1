7d033b9525142039c803bce045324ab06c5ab1a6
Jan Bernacki <releu@me.com>
2013-01-12 11:38:17 +0400

    fixes

---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,20 @@
+class RepliesController < ApplicationController
+  def create
+    @reply = Reply.new(params[:reply])
+    @reply.user = current_user
+    if current_user.ticket_ids.include?(@reply.ticket_id) && @reply.save
+      @reply.user.track! :create_reply, @reply, current_user
+      redirect_to @reply.ticket
+    else
+      @ticket = @reply.ticket
+      @replies = @ticket.replies
+      render 'tickets/show'
+    end
+  end
+
+private
+
+  def namespace
+    :dashboard
+  end
+end
