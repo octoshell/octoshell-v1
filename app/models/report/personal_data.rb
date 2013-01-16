e8c493329e94207706244caef4e486615ff612bd
Jan Bernacki <releu@me.com>
2013-01-16 19:49:32 +0400

    fix full name regexp

--- app/models/report/personal_data.rb
+++ app/models/report/personal_data.rb
@@ -3,7 +3,7 @@ class Report::PersonalData < ActiveRecord::Base
   with_options on: :update do |m|
     m.validates :first_name, :last_name, :middle_name, :email, :phone,
       presence: true
-    m.validates :first_name, :last_name, :middle_name, format: { with: /\A[A-ZА-ЯЁ]/ }
+    m.validates :first_name, :last_name, :middle_name, format: { with: /\A[A-ZА-ЯЁ\-']/ }
     m.validate :full_name_validator
     m.validates :phone, format: { with: /\A[\+\-\d\(\)\s]+\z/ }
     m.validates_email_format_of :email
@@ -18,9 +18,9 @@ private
       next if value.blank?
 
       if value =~ /[a-z]/i
-        errors.add(attr, :invalid_format) unless value =~ /\A[a-z]+\z/i
+        errors.add(attr, :invalid_format) unless value =~ /\A[a-z\-']+\z/i
       else
-        errors.add(attr, :invalid_format) unless value =~ /\A[а-яё]+\z/i
+        errors.add(attr, :invalid_format) unless value =~ /\A[а-яё\-']+\z/i
       end
     end
   end
