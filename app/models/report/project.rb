953ab97d788e365e4bc554937aa4f9fdf4a820fc
Jan Bernacki <releu@me.com>
2013-01-16 13:47:02 +0400

    add graphic file types

--- app/models/report/project.rb
+++ app/models/report/project.rb
@@ -154,7 +154,7 @@ class Report::Project < ActiveRecord::Base
       if entries.size < 2
         errors.add(:materials, :min_files_is_two)
       end
-      unless entries.find { |e| e.to_s =~ /(jpg|jpeg|png|tiff|bmp)\z/i }
+      unless entries.find { |e| e.to_s =~ /(jpg|jpeg|png|tiff|bmp|cdr|eps|ai)\z/i }
         errors.add(:materials, :no_image)
       end
       z.close
