9793d51892e524eb877ce2383714c840f06720fd
Jan Bernacki <releu@me.com>
2013-01-25 16:44:44 +0400

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -309,7 +309,7 @@ private
   end
 
   def setup_default_groups
-    user_groups.where(group_id: Group.authorized.id).find_or_create!
+    user_groups.where(group_id: Group.authorized.id).first_or_create!
     true
   end
 end
