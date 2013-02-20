3fab932fa66e88dd6f3bcbab8ffe5879cca42afc
Jan Bernacki <releu@me.com>
2013-02-20 18:32:52 +0100

    refactoring

---
+++ app/assets/javascripts/stats.js
@@ -0,0 +1,54 @@
+window.series = {}
+
+Highcharts.render = function(selector, type, series) {
+  var plotOptions;
+  if (type == 'column') {
+    plotOptions = {
+      column: {
+        dataLabels: {
+          enabled: true,
+          formatter: function() {
+            if (this.y > 0) {
+              return this.y
+            } else {
+              return ''
+            }
+          }
+        }
+      }
+    }
+  } else if (type == 'pie') {
+    plotOptions = {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          color: '#000000',
+          connectorColor: '#000000',
+          formatter: function() {
+            return '' + this.y + ' (' + this.percentage.toFixed(1) +'%)';
+          }
+        }
+      }
+    }
+  }
+  var categories = [];
+  for (i in series) {
+    categories.push(series[i].name)
+  };
+  new Highcharts.Chart({
+    chart: {
+      renderTo: selector,
+      type: type
+    },
+    title: { text: '' },
+    xAxis: {
+      categories: categories
+    },
+    yAxis: { title: { text: 'Количество' } },
+    plotOptions: plotOptions,
+    legend: false,
+    series: series
+  });
+};
