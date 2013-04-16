6923608e22f0bb466cdf16f98962a5f2f405c808
Jan Bernacki <releu@me.com>
2013-04-16 15:58:23 +0400

    fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,11 +3,11 @@ set :output, "/var/www/octoshell/shared/log/cron.log"
 ruby_path = "/home/evrone/.rbenv/versions/1.9.3-p286/bin/ruby"
 job_type :rake, "cd :path && RAILS_ENV=:environment #{ruby_path} bin/rake :task --silent :output"
 
-every 5.minutes do
+every 1.hour do
   rake "counters:active_projects"
 end
 
-every 1.minute do
+every 5.minutes do
   rake "tasks:create"
 end
 
