4500986bbbac9fd3c39cf95fa08f7a7516f24e3f
Jan Bernacki <releu@me.com>
2013-04-21 16:54:48 +0400

    cleanup

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,8 @@
-Airbrake.configure do |config|
-  config.api_key = '478461bbf74895e8e9059b0c65ee73a2'
-  config.host    = 'errbit.evrone.ru'
-  config.port    = 80
-  config.secure  = config.port == 443
+if defined?(Airbrake)
+  Airbrake.configure do |config|
+    config.api_key = '478461bbf74895e8e9059b0c65ee73a2'
+    config.host    = 'errbit.evrone.ru'
+    config.port    = 80
+    config.secure  = config.port == 443
+  end
 end
