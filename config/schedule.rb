ae74ebbb031853bf9b82da11dc176687d9140c8e
Jan Bernacki <releu@me.com>
2013-05-15 10:46:43 +0400

    fix crontasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,10 @@
 set :output, "/var/www/octoshell/shared/log/cron.log"
 
-ruby_path = "/home/evrone/.rbenv/versions/1.9.3-p286/bin/ruby"
+ruby_path = "/home/evrone/.rbenv/versions/2.0.0-p0/bin/ruby"
 job_type :rake, "cd :path && RAILS_ENV=:environment #{ruby_path} bin/rake :task --silent :output"
 
 every 5.minutes do
   rake "counters:active_projects"
-end
-
-every 1.minute do
-  rake "tasks:create"
 end
 
 every 1.hour do
