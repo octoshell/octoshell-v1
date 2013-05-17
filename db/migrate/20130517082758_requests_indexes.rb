af4cd67db78f9ec329bd6f38f16ffac100ca0cbc
Jan Bernacki <releu@me.com>
2013-05-17 12:46:56 +0400

    fix migration

--- db/migrate/20130517082758_requests_indexes.rb
+++ db/migrate/20130517082758_requests_indexes.rb
@@ -1,5 +1,7 @@
 class RequestsIndexes < ActiveRecord::Migration
   def change
+    remove_index :requests, name: :unique_pending_request_idx
+    
     ActiveRecord::Base.connection.execute %{
       CREATE UNIQUE INDEX unique_pending_request_idx ON
         requests (project_id, cluster_id)
