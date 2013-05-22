bedbf8464692a840c3f8a725bfa09854afaca5d4
Jan Bernacki <releu@me.com>
2013-05-22 15:19:18 +0400

    ui

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -13,8 +13,9 @@ class Admin::AccountsController < Admin::ApplicationController
   
   def change_confirmation
     @account = Account.find(params[:account_id])
+    old = @account.username
     if @account.change_login(params[:username])
-      redirect_to [:admin, @account.project], notice: "Логин изменен"
+      redirect_to [:admin, @account.project], notice: "Логин изменен. Не забудьте перенести данные из логина #{old}, #{@account.login}"
     else
       redirect_to [:admin, @account.project]
     end
