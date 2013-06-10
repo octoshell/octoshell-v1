4aa67ac9992deb111aff8246933b1da6f79161f4
Jan Bernacki <releu@me.com>
2013-06-10 18:34:48 +0400

    fix mailer

--- app/workers/session_data_sender.rb
+++ app/workers/session_data_sender.rb
@@ -2,7 +2,7 @@ class SessionDataSender < Struct.new(:id, :email)
   def perform
     @session = Session.find(id)
     path = create_archive!
-    zip = path[/\/(\-\w+\.zip)/, 1]
+    zip = path[/\/([\-\w]+\.zip)/, 1]
     Mailer.session_archive_is_ready(email, zip).deliver!
   end
   
