6877c99e80d57bbce23bcb149b7296f816e7754f
Jan Bernacki <releu@me.com>
2013-07-25 14:05:46 +0300

    details

--- app/scripts/cluster_check.rb
+++ app/scripts/cluster_check.rb
@@ -12,29 +12,56 @@ class ClusterCheck
     @keypath = "/tmp/octo-#{SecureRandom.hex}"
     
     @result = {
-      add_group:    nil,
-      add_user:     nil,
-      check_user:   nil,
-      block_user:   nil,
-      unblock_user: nil,
-      add_key:      nil,
-      check_key:    nil,
-      remove_key:   nil,
-      remove_user:  nil
+      add_group: {
+        cmd: "sudo /usr/octo/add_group octogroup-test",
+        expected: "ok"
+      },
+      add_user: {
+        cmd: "sudo /usr/octo/add_user octouser-#{rand} octogroup-test",
+        expected: "ok"
+      },
+      check_user: {
+        cmd: "sudo /usr/octo/check_user octouser-#{rand} octogroup-#{rand}",
+        expected: "active"
+      },
+      block_user: {
+        cmd: "sudo /usr/octo/block_user octouser-#{rand}",
+        expected: "ok"
+      },
+      unblock_user: {
+        cmd: "sudo /usr/octo/unblock_user octouser-#{rand}",
+        expected: "ok"
+      },
+      add_key: {
+        cmd: "sudo /usr/octo/add_openkey octouser-#{rand} #{@keypath}",
+        expected: "ok"
+      },
+      check_key: {
+        cmd: "sudo /usr/octo/check_openkey octouser-#{rand} #{@keypath}",
+        expected: "found"
+      },
+      remove_key: {
+        cmd: "sudo /usr/octo/del_openkey octouser-#{rand} #{@keypath}",
+        expected: "ok"
+      },
+      remove_user: {
+        cmd: "sudo /usr/octo/del_user octouser-#{rand}",
+        expected: "ok"
+      }
     }
     
-    @result[:add_group] = @connection.run("sudo /usr/octo/add_group octogroup-test")
-    @result[:add_user] = @connection.run("sudo /usr/octo/add_user octouser-#{rand} octogroup-test")
-    @result[:check_user] = @connection.run("sudo /usr/octo/check_user octouser-#{rand} octogroup-#{rand}")
-    @result[:block_user] = @connection.run("sudo /usr/octo/block_user octouser-#{rand}")
-    @result[:unblock_user] = @connection.run("sudo /usr/octo/unblock_user octouser-#{rand}")
+    @result[:add_group][:out]    = @connection.run(@result[:add_group][:cmd])
+    @result[:add_user][:out]     = @connection.run(@result[:add_user][:cmd])
+    @result[:check_user][:out]   = @connection.run(@result[:check_user][:cmd])
+    @result[:block_user][:out]   = @connection.run(@result[:block_user][:cmd])
+    @result[:unblock_user][:out] = @connection.run(@result[:unblock_user][:cmd])
     load_key!
-    @result[:add_key] = @connection.run("sudo /usr/octo/add_openkey octouser-#{rand} #{@keypath}")
+    @result[:add_key][:out]      = @connection.run(@result[:add_key][:cmd])
     load_key!
-    @result[:check_key] = @connection.run("sudo /usr/octo/check_openkey octouser-#{rand} #{@keypath}")
+    @result[:check_key][:out]    = @connection.run(@result[:check_key][:cmd])
     load_key!
-    @result[:remove_key] = @connection.run("sudo /usr/octo/del_openkey octouser-#{rand} #{@keypath}")
-    @result[:remove_user] = @connection.run("sudo /usr/octo/del_user octouser-#{rand}")
+    @result[:remove_key][:out]   = @connection.run(@result[:remove_key][:cmd])
+    @result[:remove_user][:out]  = @connection.run(@result[:remove_user][:cmd])
   end
   
   private
