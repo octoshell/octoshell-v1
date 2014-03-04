20ec414a869615f895e742309a6e499eb12a20f9
Dmitry Koprov <dmitry.koprov@gmail.com>
2014-03-04 19:09:05 +0400

    adds more submit buttons for admin on ticket reply

--- app/controllers/admin/replies_controller.rb
+++ app/controllers/admin/replies_controller.rb
@@ -6,11 +6,28 @@ class Admin::RepliesController < Admin::ApplicationController
     @reply.user = current_user
     if @reply.save
       @reply.user.track! :create_reply, @reply, current_user
-      redirect_to admin_tickets_path
+
+      if params[:show_self].present?
+        redirect_to [:admin, @reply.ticket]
+      elsif params[:show_next].present?
+        redirect_to next_ticket_path
+      else
+        redirect_to admin_tickets_path
+      end
     else
       @ticket = @reply.ticket
       @replies = @ticket.replies
       render 'tickets/show'
+    end
+  end
+
+  def next_ticket_path
+    next_ticket = @reply.ticket.find_next_ticket_from(cookies[:tickets_list])
+    if next_ticket
+      [:admin, next_ticket]
+    else
+      # прошли по всему доступному списку ранее отрисованных тикетов, показываем новый
+      admin_tickets_path
     end
   end
 end
