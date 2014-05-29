adf8036dc37f29c3b8397b6fadbc228e8b195819
Dmitry Koprov <dmitry.koprov@gmail.com>
2014-05-29 11:59:21 +0400

    creates models for city and country

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,9 @@
 class City < ActiveRecord::Base
+  belongs_to :country
 
+  scope :finder, ->(q){ where("title_ru like :q OR title_en like :q", q: "%#{q.mb_chars}%").order(:title_ru) }
+
+  def to_s
+    title_ru
+  end
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,9 @@
 class Country < ActiveRecord::Base
+  has_many :cities, inverse_of: :country, dependent: :delete_all
 
+  scope :finder, ->(q){ where("title_ru like :q OR title_en like :q", q: "%#{q.mb_chars}%").order(:title_ru) }
+
+  def to_s
+    title_ru
+  end
 end
