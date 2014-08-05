75915219e15cdef2f8b217f9d36da1ea304595ae
Dmitry Koprov <dmitry.koprov@gmail.com>
2014-08-05 09:30:53 +0400

    traling spaces fix

--- spec/features/add_member_to_project_spec.rb
+++ spec/features/add_member_to_project_spec.rb
@@ -4,12 +4,12 @@ require 'spec_helper'
 feature 'Add member to Project', js: true do
   let!(:project) { create(:project) }
   let!(:new_user) { create(:sured_user) }
-  
+
   before do
     sign_in project.user
     visit project_path(project)
   end
-  
+
   scenario 'with surety' do
     click_link 'Добавить участников'
     page.execute_script %{
@@ -29,7 +29,7 @@ feature 'Add member to Project', js: true do
     expect(page).to have_content('Участники добавлены')
     expect(page).to have_content(new_user.full_name)
   end
-  
+
   scenario 'without surety' do
     click_link 'Добавить участников'
     page.execute_script %{
@@ -50,12 +50,12 @@ feature 'Add member to Project', js: true do
     expect(page).to have_content('Участники добавлены')
     fill_in 'ФИО руководителя организации', with: 'Burns Montgomery'
     fill_in 'Должность руководителя организации', with: 'CEO'
-    
+
     click_button 'Сгенерировать'
-    
+
     # surety page
     click_on "Проект: #{project.title}"
-    
+
     # project page
     expect(page).to have_content('Flanders Nedward')
   end
