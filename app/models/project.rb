705802839a38ad113fd6c109e941afd1e010c39b
Dmitry Koprov <dmitry.koprov@gmail.com>
2014-08-05 10:21:53 +0400

    trailing spaces

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,9 @@
 class Project < ActiveRecord::Base
   CLUSTER_USER_TYPES = %w(account project)
   has_paper_trail
-  
+
   include Models::Limitable
-  
+
   belongs_to :user
   belongs_to :organization
   belongs_to :project_prefix
@@ -19,56 +19,56 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :critical_technologies
   has_and_belongs_to_many :direction_of_sciences
   has_and_belongs_to_many :research_areas
-  
+
   with_options unless: :disabled? do |enabled|
     enabled.validates :user, :organization, :title, presence: true
     enabled.validates :cluster_user_type, inclusion: { in: CLUSTER_USER_TYPES }
     enabled.validates :direction_of_science_ids, :critical_technology_ids,
       :research_area_ids, length: { minimum: 1, message: 'выберите не менее %{count}' }
   end
-  
+
   attr_accessible :organization_id, :sureties_attributes,
     :organization_ids, :direction_of_science_ids, :critical_technology_ids,
     :project_prefix_id, :research_area_ids, :title, :card_attributes
-  
+
   attr_accessible :project_prefix_id, :username, as: :admin
-  
+
   after_create :assign_username
   after_create :create_account_for_owner
   after_create :create_surety_for_owner
   before_update :mark_filled, unless: :filled?
-  
+
   accepts_nested_attributes_for :sureties, :card
-  
+
   scope :finder, lambda { |q| where("lower(title) like :q", q: "%#{q.to_s.mb_chars.downcase}%") }
   scope :enabled, where(disabled: false)
-  
+
   state_machine :state, initial: :active do
     state :active
     state :closing
     state :closed
-    
+
     event :close do
       transition :active => :closing
     end
-    
+
     event :resurrect do
       transition :closing => :active
     end
-    
+
     event :erase do
       transition :closing => :closed
     end
-    
+
     inside_transition :on => :close do |p|
       p.requests.with_state(:active).each &:block!
     end
-    
+
     inside_transition :on => :erase do |p|
       p.requests.with_state(:blocked).each &:close!
     end
   end
-  
+
   def move_to(user)
     transaction do
       self.user = user
@@ -76,16 +76,16 @@ class Project < ActiveRecord::Base
       create_account_for_owner
     end
   end
-  
+
   def disable!
     self.disabled = true
     save!
   end
-  
+
   def enabled?
     !disabled?
   end
-  
+
   def unregistered_members
     sureties.without_state(:closed).map do |surety|
       surety.surety_members.where(user_id: nil).find_all do |sm|
@@ -93,23 +93,23 @@ class Project < ActiveRecord::Base
       end
     end.flatten
   end
-  
+
   def name_with_state
     "#{title} [#{human_state_name}]"
   end
-  
+
   def allowed_organizations
     user.active_organizations
   end
-  
+
   def username=(username)
     self[:username] = username
   end
-  
+
   def as_json(options)
     { id: id, text: title }
   end
-  
+
   def build_additional_surety
     last_surety = sureties.last
     sureties.build do |surety|
@@ -126,29 +126,29 @@ class Project < ActiveRecord::Base
   def link_name
     title
   end
-  
+
   def has_active_request?
     requests.active.any?
   end
-  
+
   def has_requests?
     requests.any?
   end
-  
+
   def notify_about_blocking
     accounts.with_cluster_state(:active).each do |account|
       Mailer.delay.project_blocked(account)
     end
   end
-  
+
   def ok?
-    active? && 
+    active? &&
       requests.with_state(:active).any? &&
-      requests.with_state(:pending, :blocked).empty? && 
-      sureties.with_state(:filling, :generated, :confirmed).empty? && 
+      requests.with_state(:pending, :blocked).empty? &&
+      sureties.with_state(:filling, :generated, :confirmed).empty? &&
       accounts.with_access_state(:allowed).with_cluster_state(:closed).empty?
   end
-  
+
   def merge(project)
     transaction do
       self.organization_ids = organization_ids + project.organization_ids - [organization_id]
@@ -161,7 +161,7 @@ class Project < ActiveRecord::Base
         record.project = self
         record.save!
       end
-      
+
       project.requests.each do |request|
         request.project = self
         if requests.with_state(:active).where(cluster_id: request.cluster_id).any?
@@ -170,24 +170,24 @@ class Project < ActiveRecord::Base
           request.save!
         end
       end
-      
+
       project.disable!
     end
   end
-  
+
   def desc
     "#{title} ( #{requests.with_state(:active).map { |r| r.cluster.name }.join(', ')} )  #{accounts.pluck(:username)}"
   end
-  
+
   def users_without_surety
     finder = proc { |a| a.user.sureties.where(project_id: id).with_state(:filling, :generated, :active).empty? }
     accounts.with_access_state(:allowed).find_all(&finder).map(&:user)
   end
-  
+
   def need_new_surety?
     users_without_surety.any? && sureties.with_state(:filling).empty?
   end
-  
+
   def generate_surety_for_unsured_members
     unsured = users_without_surety
     return if unsured.empty?
@@ -198,7 +198,7 @@ class Project < ActiveRecord::Base
       surety.surety_members.where(user_id: user.id).first_or_create!
     end
   end
-  
+
   def generate_surety
     surety = sureties.with_state(:filling).first || sureties.create! do |surety|
       surety.organization = organization
@@ -207,7 +207,7 @@ class Project < ActiveRecord::Base
       surety.surety_members.where(user_id: user.id).first_or_create!
     end
   end
-  
+
   def block(desc)
     Fault.create! do |f|
       f.user = user
@@ -217,26 +217,26 @@ class Project < ActiveRecord::Base
       f.description = desc
     end
   end
-  
+
 private
-  
+
   def mark_filled
     self.filled = true
   end
-  
+
   def assign_username
     name = username? ? username : "project_#{id}"
     update_attribute :username, name
     true
   end
-  
+
   def create_account_for_owner
     conditions = { project_id: id, user_id: user_id }
     account = Account.where(conditions).first_or_create!
     account.allow! unless account.allowed?
     true
   end
-  
+
   def create_surety_for_owner
     user.revalidate!
     unless user.sured?
